2f8d3fd75f6a35a0073cc3dfa150174783629e13
Max Ramsdell <madramsdell@gmail.com>
2017-02-08 16:01:59 -0700

    First Commit
    Morgan's code, still working on... Everything... NOT FOR REFERENCE!

---
+++ src/opcontrol.c
@@ -0,0 +1,102 @@
+/** @file opcontrol.c
+ * @brief File for operator control code
+ *
+ * This file should contain the user operatorControl() function and any functions related to it.
+ *
+ * Copyright (c) 2011-2014, Purdue University ACM SIG BOTS.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Purdue University ACM SIG BOTS nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL PURDUE UNIVERSITY ACM SIG BOTS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Purdue Robotics OS contains FreeRTOS (http://www.freertos.org) whose source code may be
+ * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
+ */
+
+#include "main.h"
+
+/*
+ * Runs the user operator control code. This function will be started in its own task with the
+ * default priority and stack size whenever the robot is enabled via the Field Management System
+ * or the VEX Competition Switch in the operator control mode. If the robot is disabled or
+ * communications is lost, the operator control task will be stopped by the kernel. Re-enabling
+ * the robot will restart the task, not resume it from where it left off.
+ *
+ * If no VEX Competition Switch or Field Management system is plugged in, the VEX Cortex will
+ * run the operator control task. Be warned that this will also occur if the VEX Cortex is
+ * tethered directly to a computer via the USB A to A cable without any VEX Joystick attached.
+ *
+ * Code running in this task can take almost any action, as the VEX Joystick is available and
+ * the scheduler is operational. However, proper use of delay() or taskDelayUntil() is highly
+ * recommended to give other tasks (including system tasks such as updating LCDs) time to run.
+ *
+ * This task should never exit; it should end with some kind of infinite loop, even if empty.
+ */
+ const int rightBackDrive = 1;
+ const int rightFrontDrive = 2;
+ const int leftBackDrive = 10;
+ const int leftFrontDrive = 9;
+ const int leftTopLift = 3;
+ const int leftBottomLift = 4;
+ const int rightTopLift = 8;
+ const int rightBottomLift = 7;
+ const int clawRight = 5;
+ const int clawLeft = 6;
+
+void operatorControl() {
+
+
+	//int deadzone = 20;
+	int yAxis;
+	int xAxis;
+	int liftMove;
+
+	while (1 == 1) {
+		delay(20);
+		yAxis = joystickGetAnalog(1, 3);
+		xAxis = joystickGetAnalog(1, 4);
+		liftMove = joystickGetAnalog(1, 2);
+
+		motorSet(rightBackDrive, -yAxis + xAxis);
+		motorSet(rightFrontDrive, -yAxis + xAxis);
+		motorSet(leftTopLift, -liftMove);
+		motorSet(leftBottomLift, -liftMove);
+
+		motorSet(rightBottomLift, liftMove);
+		motorSet(rightTopLift, liftMove);
+		motorSet(leftFrontDrive, yAxis + xAxis);
+		motorSet(leftBackDrive, yAxis + xAxis);
+
+		if(joystickGetDigital(1, 6, JOY_UP)){
+			motorSet(clawRight, 127);
+			motorSet(clawLeft, -127);
+		}else if(joystickGetDigital(1, 6, JOY_DOWN)){
+			motorSet(clawRight, -127);
+			motorSet(clawLeft, 127);
+		} else {
+			motorSet(clawRight, 0);
+			motorSet(clawLeft, 0);
+		}
+
+
+	}
+}
